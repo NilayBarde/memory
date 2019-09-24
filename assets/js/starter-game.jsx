3bc25ac75d2026419d776fb7acf9ae268297e1b9
root <root@guest.guest>
2019-09-24 02:10:24 +0000

    Game Board

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -7,41 +7,17 @@ export default function game_init(root) {
 }
 
 class Starter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { left: false };
+	render() {
+	return <div>
+		<Row id={"row1"}></Row>
+	       </div>;
+	}
+
   }
-
-  swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
-    this.setState(state1);
-  }
-
-  hax(_ev) {
-    alert("hax!");
-  }
-
-  render() {
-    let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
-
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
-
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
-    }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
-    }
+class Row extends React.Component {
+  render () {
+    return <div className="gameboardrow">
+	<p>One</p>		  
+	</div>
   }
 }
-
