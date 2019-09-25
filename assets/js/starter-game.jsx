98482d7361f990a909890f67af666cf7cfc19ac4
root <root@guest.guest>
2019-09-25 00:19:21 +0000

    Board Setup

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -7,17 +7,56 @@ export default function game_init(root) {
 }
 
 class Starter extends React.Component {
-	render() {
-	return <div>
-		<Row id={"row1"}></Row>
-	       </div>;
-	}
+//Randomize an array: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 
+  boardGenerator() {
+    var lettersList = ["A", "B", "C", "D", "E", "F", "G", "H"];
+    var curr = lettersList.length, temp, randIndex;
 
+    while (0 != curr) {
+        randIndex = Math.floor(Math.random() * curr);
+	curr -= 1;
+	temp = lettersList[curr];
+	lettersList[curr] = lettersList[randIndex];
+	lettersList[randIndex] = temp;
+    }
+    return lettersList;
   }
+
+     restart() { 
+       location.reload();
+     }
+
+     render() {
+	console.log('Hello');
+	var array = this.boardGenerator();
+	var array2 = this.boardGenerator();
+	return <div class = "wrap">
+		<Row id={array[0]}></Row>
+		<Row id={array[1]}></Row>
+		<Row id={array[2]}></Row>
+		<Row id={array[3]}></Row>
+		<Row id={array[4]}></Row>
+		<Row id={array[5]}></Row>
+		<Row id={array[6]}></Row>
+		<Row id={array[7]}></Row>
+		<Row id={array2[0]}></Row>
+		<Row id={array2[1]}></Row>
+		<Row id={array2[2]}></Row>
+		<Row id={array2[3]}></Row>
+		<Row id={array2[4]}></Row>
+		<Row id={array2[5]}></Row>
+		<Row id={array2[6]}></Row>
+		<Row id={array2[7]}></Row>
+		<button onClick={this.restart}>A</button>
+	       </div>
+	}
+  }
+
 class Row extends React.Component {
+
   render () {
     return <div className="gameboardrow">
-	<p>One</p>		  
+		  <p>{this.props.id}</p>	  
 	</div>
   }
 }
